f583cbb2a37da4c6eeeaf552d6468de414eee517
coco3167 <corentinboblet@gmail.com>
2021-12-16 14:04:23 +0100

    :boom: Ajout d'un script sur la porte (pas encore effectif, système de clé non implémenté)

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    //A changer quand le player récupère la clé
+    public bool playerHasKey = false;
+    
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && playerHasKey)
+        {
+            _animator.SetTrigger("Open");
+        }
+    }
+}
